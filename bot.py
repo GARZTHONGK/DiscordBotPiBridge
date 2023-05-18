986930e4a7e9a4008258075244b234be33855d09
GARZTHONGK <39154461+GARZTHONGK@users.noreply.github.com>
2023-05-18 22:07:11 +0200

    added commands to Execute and Terminate programs also one to Execute one and kill it after a specific amount of time has passed

--- bot.py
+++ bot.py
@@ -1,26 +1,73 @@
+import multiprocessing
+
 import discord
 import responses
 from discord import app_commands
 import json
 import commands
 import subprocess
+import time
+import os
+import signal
 
 
-def get_token():
+def get_token(): # gets token from config.json
+    print("getting Token")
     with open("config.json", 'r') as file:
         data = json.loads(file.read())
     return data["token"]
 
 
+def convert_from_hours(hours):
+    return hours * 3600  # returns time in seconds
+
+
 def run_file():
     try:
-        cmdline = r"C:\Users\alber\OneDrive - Campus Vejle\Skrivebord\binmaster-v2-win.exe\binmaster-v2-win.exe~1\binmaster-v2-win-exe"
-        subprocess.run(cmdline)
+        cmdline = r"C:\Users\alber\DiscordBotPiBridge\ProgramToRun\testing exe file\Hello world.exe"
+        print("starting process")
+        process = subprocess.Popen(cmdline)
+        print("sleeping")
+        time.sleep(5)
     except Exception as e:
         print(e)
         return f"Error executing file!, {e}"
     return "Executing file succeeded"
 
+
+def start_process():
+    try:
+        cmdline = r"C:\Users\alber\DiscordBotPiBridge\ProgramToRun\testing exe file\Hello world.exe"
+        print("starting process")
+        process = subprocess.Popen(cmdline)
+        time.sleep(1)
+        print(process.pid)
+        # process.terminate()
+        return process.pid
+    except Exception as e:
+        print(e)
+        return
+
+
+def start_process_timed(hours):
+    duration = convert_from_hours(hours)
+    pid = start_process()
+    time.sleep(duration)
+    kill_process(pid)
+
+def kill_process(processpid):
+    try:
+        print(f"terminating process, pid:{processpid}")
+        os.kill(processpid, signal.SIGTERM)
+    except Exception as e:
+        print(e)
+        return
+
+
+def get_all_pids():
+    pids = multiprocessing.active_children()
+    print(pids)
+    return pids
 
 
 async def send_message(message, user_message):
@@ -37,6 +84,7 @@ def run_discord_bot():
     intents.message_content = True
     client = discord.Client(intents=intents)
     tree = app_commands.CommandTree(client)
+    processpid = None
 
     @tree.command(name="testcommand", description="My first application Command", guild=discord.Object(id=1054466267744051310))  # Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
     async def first_command(interaction):
@@ -52,10 +100,27 @@ def run_discord_bot():
         await interaction.response.send_message("Gonna sleep for 5 seconds")
         print("have slept for 5 seconds")
 
-    @tree.command(name="runexe", description="runs a specific file", guild=discord.Object(id=1054466267744051310))
+    @tree.command(name="start_program", description="runs a specific file", guild=discord.Object(id=1054466267744051310))
     async def fourth_command(interaction):
+        processpid = start_process()
 
-        await interaction.response.send_message(run_file())
+        print("pricess pid = ", processpid)
+        await interaction.response.send_message(f"Starting program PID ID:{processpid}")
+
+    @tree.command(name="kill_program", description="kills a program", guild=discord.Object(id=1054466267744051310))
+    async def fifth_command(interaction, pidid: int):
+        print("process pid = ", processpid)
+        kill_process(pidid)
+        await interaction.response.send_message("Killed program")
+
+    @tree.command(name="start_program_timed", description="runs the program for a specific amount of time", guild=discord.Object(id=1054466267744051310))
+    async def sixth_command(interaction, hours: float):
+        print("Running program for ", hours, " hours")
+        await interaction.response.send_message(f"Starting program, will end in {hours} hours")
+        start_process_timed(hours)
+
+
+
 
     @client.event
     async def on_ready():
